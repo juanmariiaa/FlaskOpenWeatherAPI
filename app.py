90d5ac6362c07e9e66ec28f8941279518d0c2e04
Juan Maria <juanmaria.ariza@nucleoo.com>
2025-04-09 16:50:20 +0200

    Added cors

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask
 from flask_smorest import Api
+from flask_cors import CORS  # Add this import
 from config import Config
 from api.weather import blp as WeatherBlueprint
 from model.models import db
 
+
 def create_app():
     app = Flask(__name__)
+    # Enable CORS for your Flask app
+    CORS(app)  # Add this line
+
     app.config.from_object(Config)
     db.init_app(app)
     api = Api(app)
     api.register_blueprint(WeatherBlueprint)
     return app
+
 
 if __name__ == '__main__':
     app = create_app()
